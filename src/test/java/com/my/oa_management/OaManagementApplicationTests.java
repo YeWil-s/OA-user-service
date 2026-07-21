bd10654fffbe2cb6cb7e3c438f3da91a2cf6ea99
YeWil-s <15151882195@163.com>
2026-07-21 08:58:54 +0800

    Initial commit: OA user service Spring Boot project

---
+++ src/test/java/com/my/oa_management/OaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.oa_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
